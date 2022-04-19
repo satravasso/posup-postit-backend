a1bbfecd213dae2017a63ee8b4a5503c1d2476b9
Rafael Giro <me@rafaelgiro.dev>
2022-04-18 22:19:42 -0300

    feat: mongo config por env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostItModule } from './postit/postit.module';
 
-const DB_USER = 'user';
-const PASSWORD = encodeURIComponent('hello@123');
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://userposup:userposup@posup-mongodb.zvffb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+      `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/?directConnection=true&serverSelectionTimeoutMS=30&appName=mongosh+1.3.1`
     ),
     PostItModule,
   ],
